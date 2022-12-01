6be27987ca275551127cf19d067efc256c6d5893
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-12-01 20:44:32 +0100

    Added the array in a different file

---
+++ js/todo-list.js
@@ -0,0 +1,22 @@
+const todos = [
+    {
+        title: "Learn basic JavaScript",
+        category: 'work',
+        completed: false
+    },
+    {
+        title: "Learn DOM",
+        category: 'work',
+        completed: false
+    },
+    {
+        title: "Go out with the dog",
+        category: 'private',
+        completed: false
+    },
+    {
+        title: "Finish my Todo-App",
+        category: 'none',
+        completed: true
+    }
+];
