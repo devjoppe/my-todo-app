5e46775884489fcf973c1842997f550b4105ce8c
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-12-01 20:46:20 +0100

    Fixed the main bug with the filter input field. Work as intended

--- js/todos-jo-03.js
+++ js/todos-jo-03.js
@@ -1,79 +1,48 @@
-// TODO:
-// DONE - Hämta in lista med icke klara todos
-// DONE - Hämta in lista med klara todos
-// DONE - Push in new Tasks
-// DONE - Click on complete
-// DONE - Delete an item
-// KINDA DONE - Filter by search box <- This was hard!
-
-const todos = [
-	{
-		title: "Learn basic JavaScript",
-        category: 'work',
-		completed: false
-	},
-	{
-		title: "Learn DOM",
-        category: 'work',
-		completed: false
-	},
-	{
-		title: "Go out with the dog",
-        category: 'private',
-		completed: false
-	},
-	{
-		title: "Finish my Todo-App",
-        category: 'none',
-		completed: true
-	}
-];
-
 const todoListApp = document.querySelector('#todolist');
 const completedListApp = document.querySelector('#completedlist');
 const addItemForm = document.querySelector('#addtodo');
 const allApp = document.querySelector('.inner-content');
 const searchForm = document.querySelector('#search');
 
-// Funktion för att hämta in och visa tasksen i listan
+// Render the list
 const getList = () => {
 
     // Clear DOM
     todoListApp.innerHTML = ``;
     completedListApp.innerHTML = ``;
 
-    const upcomingTask = todos.filter(taskItem => taskItem.completed == false);
+    // Upcoming tasks
+    const upcomingTask = todos.filter(taskItem => taskItem.completed === false);
 
     upcomingTask.forEach(listItem => {
-        // List onging tasks template
-
         const listDataItems = `
         <div class="listitem ongoing" data-title="${listItem.title}">
             <div class="itemcontent">
                 <div class="check">
-                    <span class="material-symbols-outlined" data="${listItem.title}">done</span>
+                    <span class="material-symbols-outlined" data-done="${listItem.title}">done</span>
                 </div>
                 <span class="itemtitle">${listItem.title}</span>
             </div>
             <div class="misc">
                 <span class="category ${listItem.category}">${listItem.category}</span>
-                <span class="material-symbols-outlined trash" delete="${listItem.title}">delete</span>
+                <span class="material-symbols-outlined trash" data-delete="${listItem.title}">delete</span>
             </div>
         </div>
         `;
         // Connect it to the DOM
         todoListApp.innerHTML += listDataItems;
     })
+
     // Completed tasks
-    const compTasks = todos.filter(taskItems => taskItems.completed == true);
+    const compTasks = todos.filter(taskItems => taskItems.completed === true);
 
     compTasks.forEach(listItem => {
         const listDataItems = `
-        <div class="listitem completed">
+        <div class="listitem completed" data-title="${listItem.title}">
             <span>${listItem.title}</span>
             <div class="misc">
                 <span class="category ${listItem.category}">${listItem.category}</span>
-                <span class="material-symbols-outlined trash" delete="${listItem.title}">delete</span>
+                <span class="material-symbols-outlined trash" data-delete="${listItem.title}">delete</span>
             </div>
         </div>
         `;
@@ -104,39 +73,35 @@ addItemForm.addEventListener('submit', (e) => {
 
 allApp.addEventListener('click', (e) => {
 
-    if(e.target.getAttribute('data')) {
-        
-        const listItem = e.target.getAttribute('data');
-        //Look into the objekt with the title who has the same name as the button attribute
-        const getItem = todos.find(({ title }) => title === listItem);
+    //Look into the objekt with the title who has the same name as the button attribute
+    if(e.target.dataset.done) {
+        const getItem = todos.find(({ title }) => title === e.target.dataset.done);
         getItem.completed = true;
     }
-
-    if(e.target.getAttribute('delete')) {
-        const listItem = e.target.getAttribute('delete');
-        // Get the index in of the object in todos
-        const getItem = todos.findIndex(item => item.title === listItem);
+    if(e.target.dataset.delete) {
+        const getItem = todos.findIndex(item => item.title === e.target.dataset.delete);
         // Remove the object from the array
         todos.splice(getItem, 1);
     }
+    removeFilter();
     getList();
 });
 
-const listContent = document.querySelectorAll('.itemtitle');
-
-// Filter function with some bugs :)
+// The filter function
 const filterItems = searchTerm => {
     const itemEl = document.querySelectorAll('.listitem');
-    for(i = 0; i < itemEl.length; i++) {
-        itemEl[i].classList.add('hide');
-    }
+
+    itemEl.forEach(listItem => {
+        if(!listItem.classList.contains('completed')) {
+            listItem.classList.add('hide');
+        }
+    })
     
-    const taskFilter = todos.filter((filterItem) => filterItem.title.includes(searchTerm));
+    const taskFilter = todos.filter((filterItem) => filterItem.title.toLowerCase().trim().includes(searchTerm));
 
     taskFilter.forEach(listItem => {
         let filteredEl = document.querySelector('[data-title="' + listItem.title + '"]')
-        if(filteredEl == null) {
-        } else {
+        if(!listItem.completed) {
             filteredEl.classList.remove('hide');
         }
     });
@@ -144,8 +109,11 @@ const filterItems = searchTerm => {
 
 searchForm.addEventListener('keyup', (e) => {
     e.preventDefault();
-    const searchOutput = searchForm.searchfield.value.trim();
-    
-    // Starts the filter function
+    const searchOutput = searchForm.searchfield.value.toLowerCase().trim();
     filterItems(searchOutput); 
 });
+
+// Function remove filter text
+const removeFilter = () => {
+    searchForm.searchfield.value = ``;
+}
