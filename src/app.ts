d67a0eab069d9986dd286be2c573256f4cfc8a93
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-12-13 09:16:52 +0100

    update with new database, firebase and API

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,98 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import './assets/style/css/style.min.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Firebase Imports: getFirestore, collection and getDocs
+import {initializeApp} from 'firebase/app'
+import {
+    collection,
+    getFirestore,
+    addDoc,
+    deleteDoc,
+    doc,
+    onSnapshot,
+    query,
+    orderBy,
+    serverTimestamp
+    } from 'firebase/firestore'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// API Config
+const firebaseConfig = {
+    apiKey: "AIzaSyCJkA1zTwyjODtEb74edHblI3eZhuyPd1s",
+    authDomain: "my-todo-4c6e2.firebaseapp.com",
+    projectId: "my-todo-4c6e2",
+    storageBucket: "my-todo-4c6e2.appspot.com",
+    messagingSenderId: "107145830573",
+    appId: "1:107145830573:web:f320161aa9f73f28222624"
+};
+
+// Init API
+initializeApp(firebaseConfig)
+
+// Connect to database
+const db = getFirestore()
+
+// Select collection in Firebase
+const colRef = collection(db, 'todos')
+
+interface todosItem {
+    todo: string,
+    completed: boolean,
+    id: any,
+    category: string,
+    created: any
+}
+
+// Query
+const q = query(colRef, orderBy('created', 'desc'))
+
+let todos: todosItem []
+
+// Fetch/update data from Firebase realtime
+onSnapshot(q, (snapshot) => {
+    todos = []
+    snapshot.docs.forEach(item => {
+        todos.push({
+            id: item.id,
+            todo: item.data().todo,
+            completed: item.data().completed,
+            category: item.data().category,
+            created: serverTimestamp()
+        })
+    })
+    console.log(todos)
+    renderTodos()
+})
+
+// Render
+const renderTodos = () => {
+    document.querySelector('#todolist')!.innerHTML = todos.filter(item => !item.completed).map(item => {
+        return `<div class="listitem ongoing" data-title="${item.todo}">
+            <div class="itemcontent">
+                <div class="check">
+                    <span class="material-symbols-outlined" data-done="${item.todo}">done</span>
+                </div>
+                <span class="itemtitle">${item.todo}</span>
+            </div>
+            <div class="misc">
+                <span class="category ${item.category}">${item.category}</span>
+                <span class="material-symbols-outlined trash" data-delete="${item.id}">delete</span>
+            </div>
+        </div>`
+    }).join('')
+}
+
+// Add
+const todoForm = document.querySelector('#addtodo') as HTMLFormElement
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addDoc(colRef, {
+        todo: todoForm.addtask.value,
+        category: todoForm.taskcategory.value,
+        completed: false,
+        created: serverTimestamp()
+    })
+    .then(() => {
+        todoForm.reset()
+    })
+})
+
+// Delete
